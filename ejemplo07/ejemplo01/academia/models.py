929ee9155e9dc98da0107befc43c135f0722f77f
ANTONELLA DEL CISNE PARRA CARRION <adparra2@utpl.edu.ec>
2026-06-11 12:12:35 -0500

    CAMBIOS CLASE

--- ejemplo07/ejemplo01/academia/models.py
+++ ejemplo07/ejemplo01/academia/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from datetime import date
 
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)
@@ -8,11 +7,12 @@ class Estudiante(models.Model):
     cedula = models.CharField(max_length=30, unique=True)
     edad = models.IntegerField()
 
+    def __str__(self):
+        anio_nacimiento = date.today().year - self.edad
 
- #    def __str__(self):
- #      return "%s - %s - %s - edad: %d" % (self.nombre, 
-  #              self.apellido,
-   #             self.cedula,
-    #            self.edad)
-def __str__(self):
-    return f"{self.nombre} - {self.apellido} - {self.cedula} - edad: {self.edad}"
+        if self.cedula.startswith("11"):
+            ciudad = "Loja"
+        else:
+            ciudad = "Otra ciudad"
+
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - CI: {ciudad} - Edad: {self.edad} - Año de nacimiento: {anio_nacimiento}"
